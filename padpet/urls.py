c3ef99d8e791b3cdb0a6e56901b715b7cc046193
leonardosoler <leonardo-leoss@hotmail.com>
2022-12-31 12:58:17 -0300

    [DEV-0001] Adjustments ins the name of the django project.

--- padpet/urls.py
+++ padpet/urls.py
@@ -1,4 +1,4 @@
-"""padped URL Configuration
+"""padpet URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from main.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', HomePageView.as_view(), name = 'home'),
+    path("register/", register_request, name="register")
+
 ]
