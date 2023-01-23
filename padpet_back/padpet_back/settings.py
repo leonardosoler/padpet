ff05e29b944fd32e78ea335bb38a5ee3a5a12496
leonardosoler <leonardo-leoss@hotmail.com>
2023-01-22 22:02:57 -0300

    [alpha] fix: ajustes nas migrações para o postgres

--- padpet_back/padpet_back/settings.py
+++ padpet_back/padpet_back/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'rest_framework',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -120,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
