16ad0b8573088f14c08c4a9b9349482db6fbc427
leonardosoler <leonardo-leoss@hotmail.com>
2023-02-05 21:57:55 -0300

    [DEV-0005] feat: User, Address and local models created.

--- padpet_back/local/models.py
+++ padpet_back/local/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User, Group
+from pet.models import Pet
+class Address(models.Model):
+    id = models.AutoField(primary_key=True)
+    street = models.CharField()
+    city = models.CharField()
+    complement = models.CharField()
+    state = models.CharField()
+    zip_code = models.CharField()
 
-# Create your models here.
+    class Meta:
+        app_label = 'local'
+class Local(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    address = models.ForeignKey(Address, verbose_name=("Endereço"))
+    user = models.OneToOneField(User, verbose_name=("Usuário associado"))
+    pet = models.ForeignKey(Pet, verbose_name=("Pets"))
+
+    class Meta:
+        app_label = 'local'
--- padpet_back/user/models.py
+++ padpet_back/user/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from main.models import BaseModel
+from pet.models import Pet
+from local.models import Local
 # Create your models here.
+class UserProfileInfos(BaseModel):
+    id = models.AutoField(primary_key=True)
+    full_name = models.CharField(db_column='full_name', max_length=255, verbose_name =("Nome completo"))
+    document = models.IntegerField(verbose_name=("CPF"))
+    tel = models.CharField(verbose_name="Telefone", max_length=11)
+    pet = models.ManyToManyField(Pet, verbose_name=("Pets"))
+    local = models.ManyToManyField(Local, verbose_name=("Local"))
+    email = models.EmailField(verbose_name=("Email"))
+    address = models.ManyToManyField(Local, verbose_name=("Local"))
+
+    class Meta:
+        app_label = 'user'
