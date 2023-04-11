9f93eb3dffeb74924eed2a43aa2183039a4c07e2
leonardosoler <leonardo-leoss@hotmail.com>
2023-04-10 23:46:53 -0300

    [DEV-0006] test: Pe form test

--- padpet_back/pet/models.py
+++ padpet_back/pet/models.py
@@ -2,22 +2,11 @@ from django.db import models
 # from main.models import  Teste
 # Create your models here.
 from main.models import BaseModel
-class Race (BaseModel):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(db_column='name', max_length=255)
 
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        managed = True
-        db_table = 'race'
-        app_label = 'pet'
 
 class Specie (BaseModel):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(db_column='name', max_length=255)
-    race = models.ManyToManyField(Race, verbose_name="race", related_name="specie_race_set")
+    name = models.CharField(db_column='name',  max_length=255)
 
     def __str__(self):
         return self.name
@@ -25,6 +14,18 @@ class Specie (BaseModel):
     class Meta:
         managed = True
         db_table = 'specie'
+        app_label = 'pet'
+
+class Race (BaseModel):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(db_column='name', max_length=255)
+    specie = models.ForeignKey(Specie, verbose_name="specie", on_delete=models.SET_NULL, null = True, related_name="specie_race_set") 
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        managed = True
+        db_table = 'race'
         app_label = 'pet'
 
 class Pet (BaseModel):
